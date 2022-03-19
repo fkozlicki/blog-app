a16252a907c04da77e4e0ca6b6408c7edc565f8f
Filip Kozlicki <filip.kozlickii@gmail.com>
2022-03-19 21:54:16 +0100

    connected form to emailjs

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -5,10 +5,66 @@ import {
 	faMessage,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRef, useState } from "react";
+import emailjs from "emailjs-com";
 
 const ContactForm = () => {
+	const [showSuccessMessage, setShowSuccessMessage] = useState("");
+	const [error, setError] = useState("");
+	const formRef = useRef();
+	const nameRef = useRef();
+	const emailRef = useRef();
+	const messageRef = useRef();
+
+	const isEmail = (email) => {
+		return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+			email
+		);
+	};
+
+	const sendEmail = (e) => {
+		e.preventDefault();
+		setError(false);
+
+		const { value: name } = nameRef.current;
+		const { value: email } = emailRef.current;
+		const { value: message } = messageRef.current;
+
+		if (!message || !name || !email) {
+			setError("Wszystkie pola są wymagane");
+			return;
+		} else if (!isEmail(email)) {
+			setError("Niepoprawny adres email");
+			return;
+		}
+
+		emailjs
+			.sendForm(
+				"service_7978xqs",
+				"template_e0y03qo",
+				formRef.current,
+				"user_NrdjZ2gZBWolYyWeHp5hr"
+			)
+			.then(
+				(result) => {
+					setShowSuccessMessage(true);
+					setTimeout(() => {
+						setShowSuccessMessage(false);
+					}, 3000);
+					nameRef.current.value = "";
+					emailRef.current.value = "";
+					messageRef.current.value = "";
+				},
+				(error) => {
+					setError(true);
+				}
+			);
+	};
+
 	return (
-		<div
+		<form
+			ref={formRef}
+			onSubmit={sendEmail}
 			className="p-3 position-relative rounded-3"
 			style={{
 				maxWidth: "500px",
@@ -20,11 +76,12 @@ const ContactForm = () => {
 			<MDBRow className="gy-3">
 				<MDBCol size="12" md="6">
 					<MDBInput
+						inputRef={nameRef}
 						id="name"
 						size="lg"
 						type="text"
 						label="Imię"
-						name="name"
+						name="from_name"
 						className="pe-4 position-relative"
 					>
 						<FontAwesomeIcon
@@ -36,11 +93,12 @@ const ContactForm = () => {
 				</MDBCol>
 				<MDBCol size="12" md="6">
 					<MDBInput
+						inputRef={emailRef}
 						className="pe-4 position-relative"
 						label="Email"
 						id="email"
 						type="email"
-						name="email"
+						name="user_email"
 						size="lg"
 					>
 						<FontAwesomeIcon
@@ -52,6 +110,7 @@ const ContactForm = () => {
 				</MDBCol>
 				<MDBCol size="12">
 					<MDBInput
+						inputRef={messageRef}
 						id="message"
 						label="Wiadomość"
 						size="lg"
@@ -68,11 +127,15 @@ const ContactForm = () => {
 						/>
 					</MDBInput>
 				</MDBCol>
-				<MDBCol>
-					<MDBBtn>Wyślij</MDBBtn>
+				<MDBCol className="d-flex align-items-center justify-content-between">
+					<MDBBtn type="submit">Wyślij</MDBBtn>
+					{error && <div className="text-danger fw-bold">{error}</div>}
+					{showSuccessMessage && (
+						<div className="text-success fw-bold">Wysłano pomyślnie</div>
+					)}
 				</MDBCol>
 			</MDBRow>
-		</div>
+		</form>
 	);
 };
 
