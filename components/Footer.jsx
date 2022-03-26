1b9c72a883645d144c23ff1288786fbdb41b6b9d
Filip Kozlicki <filip.kozlickii@gmail.com>
2022-03-26 20:14:40 +0100

    Logo

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,33 +2,27 @@ import Link from "next/link";
 import { MDBContainer, MDBRow, MDBCol, MDBTypography } from "mdb-react-ui-kit";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope, faGlobe } from "@fortawesome/free-solid-svg-icons";
+import logo from "../public/logo1.png";
 
 const Footer = () => {
 	return (
 		<footer className="pt-5 pb-2 ">
 			<MDBContainer>
 				<MDBRow className="align-items-center gy-4">
-					<MDBCol size="12" md="6">
+					<MDBCol size="12" md="4" lg="6">
 						<a
 							href="#header"
 							className="my-link display-4 d-block fw-normal text-center m-0"
 						>
-							Programista HTML
+							<img src={logo} alt="logo" className="img-fluid" />
 						</a>
 					</MDBCol>
 					<MDBCol
 						size="12"
-						md="3"
-						className="order-md-first text-center text-md-start"
+						md="4"
+						lg="3"
+						className="text-center order-md-first text-md-start"
 					>
-						<Link href="/contact">
-							<a className="my-link d-block">Kontakt</a>
-						</Link>
-						<Link href="/blog">
-							<a className="my-link d-block">Blog</a>
-						</Link>
-					</MDBCol>
-					<MDBCol size="12" md="3" className="text-center text-md-end">
 						<p className="m-0 fs-6 ">
 							<FontAwesomeIcon icon={faEnvelope} width="16" className="me-2" />
 							filip.kozlickii@gmail.com
@@ -38,9 +32,17 @@ const Footer = () => {
 							www.kozlicki.com
 						</a>
 					</MDBCol>
+					<MDBCol size="12" md="4" lg="3" className="text-center text-md-end">
+						<Link href="/contact">
+							<a className="my-link d-block">Kontakt</a>
+						</Link>
+						<Link href="/blog">
+							<a className="my-link d-block">Blog</a>
+						</Link>
+					</MDBCol>
 				</MDBRow>
 
-				<MDBTypography className="text-center m-0 mt-4">
+				<MDBTypography className="text-center m-0 mt-6">
 					<small>@copyright All rights reserved.</small>
 				</MDBTypography>
 			</MDBContainer>
