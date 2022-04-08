4667a44d91e38ef86a5904139719530d6598610b
Filip Kozlicki <filip.kozlickii@gmail.com>
2022-04-08 16:07:19 +0200

    logo

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,6 +1,6 @@
 import { MDBTypography, MDBBtn } from "mdb-react-ui-kit";
 import image from "../public/bg.webp";
-import logo from "../public/logo2.png";
+import logo from "../public/light1.svg";
 
 const Hero = () => {
 	return (
@@ -18,17 +18,17 @@ const Hero = () => {
 					background: "rgba(0, 0, 0, 0.7)",
 				}}
 			>
-				<div className="px-3 d-flex flex-column gap-4 text-white">
+				<div className="px-3 d-flex flex-column text-white">
 					<img src={logo} alt="logo" className="img-fluid" />
-					<MDBTypography tag="div" className="fs-3 fw-normal">
+					<MDBTypography tag="div" className="fs-3 fw-normal mt-5">
 						Dowiedz się więcej o frontendzie rezem z nami.
 					</MDBTypography>
 					<MDBBtn
 						tag="a"
 						rounded
+						size="lg"
 						href="#recent"
-						className="align-self-center"
-						style={{ padding: ".75rem 1.4rem", fontSize: ".835rem" }}
+						className="align-self-center mt-6"
 					>
 						Najnowsze artykuły
 					</MDBBtn>
