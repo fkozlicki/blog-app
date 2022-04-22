6f866864e7852ccb550ad047a003ff348a1ada31
Filip Kozlicki <filip.kozlickii@gmail.com>
2022-04-22 22:25:01 +0200

    new bootstrap grid layout

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,43 @@
 import {
 	Navbar,
 	Hero,
-	About,
 	Recent,
-	Contact,
 	Footer,
 	Meta,
+	PostCard,
+	PostWidget,
+	Categories,
+	Card,
+	ContactForm,
 } from "../components/";
 import { getPosts } from "../services";
+import { MDBContainer, MDBCol, MDBRow } from "mdb-react-ui-kit";
 
 export default function Home({ posts }) {
 	return (
 		<>
 			<Meta />
-			<header id="header">
+			<div className="app">
 				<Navbar />
 				<Hero />
-			</header>
-			<main className="bg-background">
-				<About />
-				<Recent posts={posts} />
-				<Contact />
-			</main>
-			<Footer />
+				<main>
+					<MDBContainer>
+						<MDBRow between>
+							<MDBCol size={12} lg={8} className="py-6">
+								<Recent posts={posts} />
+							</MDBCol>
+							<MDBCol size={12} lg={3} className="py-6">
+								<Categories />
+							</MDBCol>
+							<MDBCol size={12} lg={8} className="py-6">
+								<ContactForm />
+							</MDBCol>
+						</MDBRow>
+					</MDBContainer>
+				</main>
+
+				<Footer />
+			</div>
 		</>
 	);
 }
