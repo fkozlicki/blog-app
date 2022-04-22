d9df0d0c7a656713423a1de4dbdaff08834564c9
Filip Kozlicki <filip.kozlickii@gmail.com>
2022-04-22 22:24:33 +0200

    title and different bootstrap grid layout

--- components/Recent.jsx
+++ components/Recent.jsx
@@ -1,32 +1,30 @@
-import { MDBTypography, MDBContainer, MDBCol, MDBRow } from "mdb-react-ui-kit";
+import { MDBCol, MDBRow } from "mdb-react-ui-kit";
 import Card from "./Card";
 
 const Recent = ({ posts }) => {
 	return (
-		<section className="py-6 py-md-7" id="recent">
-			<MDBContainer>
-				<MDBTypography
-					tag="h1"
-					className="display-6 text-uppercase text-center mb-5 mb-md-7 pb-2 border-bottom"
-				>
-					Ostatnie Posty
-				</MDBTypography>
-				<MDBRow className="justify-content-center gy-4 gy-lg-6">
-					{posts.map((post, i) => (
-						<MDBCol md="5" lg="4" key={i}>
-							<Card
-								img={post.node.featuredImage.url}
-								title={post.node.title}
-								excerpt={post.node.excerpt}
-								author={post.node.author.name}
-								date={post.node.createdAt}
-								slug={post.node.slug}
-							/>
-						</MDBCol>
-					))}
-				</MDBRow>
-			</MDBContainer>
-		</section>
+		<>
+			<h1
+				className="fs-5 text-uppercase text-secondary mb-6 text-center text-md-start"
+				stle={{ fontWeight: 500 }}
+			>
+				Ostatnie posty
+			</h1>
+			<MDBRow className="gy-4">
+				{posts.map((post, i) => (
+					<MDBCol key={i} size={12} md={6}>
+						<Card
+							img={post.node.featuredImage.url}
+							title={post.node.title}
+							excerpt={post.node.excerpt}
+							author={post.node.author.name}
+							date={post.node.createdAt}
+							slug={post.node.slug}
+						/>
+					</MDBCol>
+				))}
+			</MDBRow>
+		</>
 	);
 };
 
