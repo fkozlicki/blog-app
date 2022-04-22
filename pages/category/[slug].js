8264d609a493f354b3c2955143443cc7f59aca33
Filip Kozlicki <filip.kozlickii@gmail.com>
2022-04-22 22:25:37 +0200

    changed PostCard

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -1,23 +1,40 @@
 import React from "react";
 
 import { getCategories, getCategoryPost } from "../../services";
-import { Navbar, PostCard, Categories, Meta } from "../../components";
+import { Navbar, Card, Categories, Meta } from "../../components";
 import { MDBContainer, MDBCol, MDBRow } from "mdb-react-ui-kit";
 
-const CategoryPost = ({ posts }) => {
+const CategoryPosts = ({ posts }) => {
 	return (
 		<>
 			<Meta title="SmartDev" />
 			<Navbar />
-			<main className="pt-6 pt-md-7 min-vh-100">
+			<main className="pt-7 pt-md-5 pb-4 min-vh-100">
 				<MDBContainer>
-					<MDBRow>
-						<MDBCol size="12" lg="8">
-							{posts.map((post, index) => (
-								<PostCard key={index} post={post.node} />
-							))}
+					<MDBRow between className="gy-5">
+						<MDBCol size={12} md={8}>
+							<h1
+								className="fs-5 text-uppercase text-secondary mb-6 text-center text-md-start"
+								stle={{ fontWeight: 500 }}
+							>
+								Ostatnie posty
+							</h1>
+							<MDBRow>
+								{posts.map((post, i) => (
+									<MDBCol key={i} size={12} md={6}>
+										<Card
+											img={post.node.featuredImage.url}
+											title={post.node.title}
+											excerpt={post.node.excerpt}
+											author={post.node.author.name}
+											date={post.node.createdAt}
+											slug={post.node.slug}
+										/>
+									</MDBCol>
+								))}
+							</MDBRow>
 						</MDBCol>
-						<MDBCol size="12" lg="4">
+						<MDBCol size={12} lg={3}>
 							<Categories />
 						</MDBCol>
 					</MDBRow>
@@ -26,7 +43,7 @@ const CategoryPost = ({ posts }) => {
 		</>
 	);
 };
-export default CategoryPost;
+export default CategoryPosts;
 
 // Fetch data at build time
 export async function getStaticProps({ params }) {
