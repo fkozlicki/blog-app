a02045aa1aed8cf107e79a07fc28f5ce3eecdb2f
Filip Kozlicki <filip.kozlickii@gmail.com>
2022-04-22 22:23:34 +0200

    imports

--- components/index.js
+++ components/index.js
@@ -1,10 +1,8 @@
-export { default as About } from "./About";
 export { default as Card } from "./Card";
 export { default as Hero } from "./Hero";
 export { default as Layout } from "./Layout";
 export { default as Navbar } from "./Navbar";
 export { default as Recent } from "./Recent";
-export { default as Contact } from "./Contact";
 export { default as Footer } from "./Footer";
 export { default as PostCard } from "./PostCard";
 export { default as PostWidget } from "./PostWidget";
