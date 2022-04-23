72a7ccd972cfe495e0170d5920ad8587ce6eb6c2
Filip Kozlicki <filip.kozlickii@gmail.com>
2022-04-23 20:29:10 +0200

    new navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,9 +6,14 @@ import {
 	MDBNavbar,
 	MDBNavbarBrand,
 	MDBNavbarNav,
-	MDBNavbarLink,
 	MDBCollapse,
 } from "mdb-react-ui-kit";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faGithub,
+	faTwitter,
+	faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Navbar = () => {
 	const [isOpen, setIsOpen] = useState(false);
@@ -33,7 +38,7 @@ const Navbar = () => {
 				data-mdb-animation="fade-in"
 				data-mdb-toggle="animation"
 				data-mdb-animation-reset="true"
-				className={`fixed-top bg-primary shadow-none `}
+				className="fixed-top bg-primary shadow-none"
 				expand="md"
 				light
 			>
@@ -46,7 +51,7 @@ const Navbar = () => {
 						</Link>
 					</MDBNavbarBrand>
 					<button
-						className="menuBtn navbar-toggler"
+						className="navbar-toggler"
 						type="button"
 						data-toggle="collapse"
 						aria-expanded="false"
@@ -61,12 +66,46 @@ const Navbar = () => {
 							<span className="line"></span>
 						</div>
 					</button>
-					<MDBCollapse id="navbarCollapse3" show={isOpen} navbar>
-						<MDBNavbarNav left className="justify-content-md-end">
-							<MDBNavbarLink href="/">Home</MDBNavbarLink>
-							<MDBNavbarLink href="/blog">Blog</MDBNavbarLink>
-							<MDBNavbarLink href="/contact">Kontakt</MDBNavbarLink>
+					<MDBCollapse id="navbarCollapse" show={isOpen} navbar>
+						<MDBNavbarNav left>
+							<Link href="/">
+								<a className="py-2 px-md-3 fw-bold link">Home</a>
+							</Link>
+							<Link href="/#posts">
+								<a className="py-2 px-md-3 fw-bold link">Posty</a>
+							</Link>
+							<Link href="/#contact">
+								<a className="py-2 px-md-3 fw-bold link">Kontakt</a>
+							</Link>
 						</MDBNavbarNav>
+						<div className="d-flex justify-content-center gap-4 py-2">
+							<a
+								href="https://github.com/fkozlicki"
+								className="d-inline-flex gap-2 link"
+								target="_blank"
+								rel="noreferrer noopener"
+							>
+								<FontAwesomeIcon width={20} icon={faGithub} />
+							</a>
+
+							<a
+								href="https://twitter.com/FKozlicki"
+								className="d-inline-flex gap-2 link"
+								target="_blank"
+								rel="noreferrer noopener"
+							>
+								<FontAwesomeIcon width={20} icon={faTwitter} />
+							</a>
+
+							<a
+								href="https://www.linkedin.com/in/fkozlicki/"
+								target="_blank"
+								rel="noreferrer noopener"
+								className="d-inline-flex gap-2 link"
+							>
+								<FontAwesomeIcon width={20} icon={faLinkedin} />
+							</a>
+						</div>
 					</MDBCollapse>
 				</MDBContainer>
 			</MDBNavbar>
