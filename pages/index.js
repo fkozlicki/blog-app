b5c3fbb1128bf8ac1a325c4e02743c1e653d675c
Filip Kozlicki <filip.kozlickii@gmail.com>
2022-04-23 20:31:37 +0200

    imports, paddings and ids for navigation

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,4 @@
-import {
-	Navbar,
-	Hero,
-	Recent,
-	Footer,
-	Meta,
-	PostCard,
-	PostWidget,
-	Categories,
-	Card,
-	ContactForm,
-} from "../components/";
+import { Hero, Recent, Meta, Categories, ContactForm } from "../components/";
 import { getPosts } from "../services";
 import { MDBContainer, MDBCol, MDBRow } from "mdb-react-ui-kit";
 
@@ -17,27 +6,20 @@ export default function Home({ posts }) {
 	return (
 		<>
 			<Meta />
-			<div className="app">
-				<Navbar />
-				<Hero />
-				<main>
-					<MDBContainer>
-						<MDBRow between>
-							<MDBCol size={12} lg={8} className="py-6">
-								<Recent posts={posts} />
-							</MDBCol>
-							<MDBCol size={12} lg={3} className="py-6">
-								<Categories />
-							</MDBCol>
-							<MDBCol size={12} lg={8} className="py-6">
-								<ContactForm />
-							</MDBCol>
-						</MDBRow>
-					</MDBContainer>
-				</main>
-
-				<Footer />
-			</div>
+			<Hero />
+			<MDBContainer>
+				<MDBRow between>
+					<MDBCol size={12} lg={8} id="posts" className="pb-6 pt-6">
+						<Recent posts={posts} />
+					</MDBCol>
+					<MDBCol size={12} lg={3} className="pb-6 pt-md-6">
+						<Categories />
+					</MDBCol>
+					<MDBCol size={12} lg={8} id="contact" className="pb-6">
+						<ContactForm />
+					</MDBCol>
+				</MDBRow>
+			</MDBContainer>
 		</>
 	);
 }
