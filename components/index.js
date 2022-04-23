fee382b8513b5fa51d21b39f48c98005da18c592
Filip Kozlicki <filip.kozlickii@gmail.com>
2022-04-23 20:28:22 +0200

    exports

--- components/index.js
+++ components/index.js
@@ -4,7 +4,6 @@ export { default as Layout } from "./Layout";
 export { default as Navbar } from "./Navbar";
 export { default as Recent } from "./Recent";
 export { default as Footer } from "./Footer";
-export { default as PostCard } from "./PostCard";
 export { default as PostWidget } from "./PostWidget";
 export { default as Categories } from "./Categories";
 export { default as PostDetail } from "./PostDetail";
