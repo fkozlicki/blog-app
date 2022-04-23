5943e3c68015d9407a5a32fb3c2cd7614dbbf0e1
Filip Kozlicki <filip.kozlickii@gmail.com>
2022-04-23 20:30:26 +0200

    Image & Link - next.js

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,51 +1,54 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import Image from "next/image";
 import moment from "moment";
 
-import { getSimilarPosts, getRecentPosts } from "../services";
+import { getSimilarPosts } from "../services";
 
 const PostWidget = ({ categories, slug }) => {
 	const [relatedPosts, setRelatedPosts] = useState([]);
 
 	useEffect(() => {
+		let abortController = new AbortController();
 		if (slug) {
 			getSimilarPosts(categories, slug).then((result) => {
 				setRelatedPosts(result);
 			});
-		} else {
-			getRecentPosts().then((result) => {
-				setRelatedPosts(result);
-			});
 		}
-	}, [slug, categories]);
+		return () => {
+			abortController.abort();
+		};
+	}, [categories, slug]);
 
 	return (
-		<div className="shadow-lg rounded-3 p-4 mb-4 bg-white">
-			<h1 className="fs-3 mb-2 font-semibold border-bottom border-1 border-gray pb-1">
-				{slug ? "Powiązane Posty" : "Ostatnie Posty"}
-			</h1>
-			<div className="d-flex flex-column gap-2">
-				{relatedPosts.map((post, index) => (
-					<div key={index} className="d-flex align-items-center w-100 gap-3">
-						<img
-							alt={post.title}
-							src={post.featuredImage.url}
-							className="img-fluid rounded"
-							width="70"
-							height="70"
-						/>
-
-						<div className="flex-grow-1">
-							<p className="text-dark m-0 fs-6 fw-bold">
-								{moment(post.createdAt).format("DD MMM YYYY")}
-							</p>
-							<a href={`/post/${post.slug}`} key={index} className="my-link">
-								{post.title}
-							</a>
+		relatedPosts.length > 0 && (
+			<section className="mb-5">
+				<h1 className="fs-3 mb-3 font-semibold border-bottom border-1 border-gray pb-2">
+					{slug ? "Powiązane Posty" : "Ostatnie Posty"}
+				</h1>
+				<div className="d-flex flex-column gap-2">
+					{relatedPosts.map((post, index) => (
+						<div key={index} className="d-flex align-items-center w-100 gap-3">
+							<Image
+								alt={post.title}
+								src={post.featuredImage.url}
+								width={80}
+								height={45}
+								className="rounded"
+							/>
+							<div className="flex-grow-1">
+								<p className="text-dark m-0 fs-6 fw-bold">
+									{moment(post.createdAt).format("DD MMM YYYY")}
+								</p>
+								<Link href={`/post/${post.slug}`}>
+									<a className="link">{post.title}</a>
+								</Link>
+							</div>
 						</div>
-					</div>
-				))}
-			</div>
-		</div>
+					))}
+				</div>
+			</section>
+		)
 	);
 };
 
