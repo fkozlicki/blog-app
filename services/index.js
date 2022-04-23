7caee2910f6abc9ba6a04831ef1972651869378a
Filip Kozlicki <filip.kozlickii@gmail.com>
2022-04-23 20:33:05 +0200

    optimizing redundant code - isEmail function

--- services/index.js
+++ services/index.js
@@ -197,3 +197,9 @@ export const getCategoryPost = async (slug) => {
 
 	return result.postsConnection.edges;
 };
+
+export const isEmail = (email) => {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+		email
+	);
+};
