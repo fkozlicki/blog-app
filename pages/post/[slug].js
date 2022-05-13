3822468910c1f1510deb4d6f58792b9c5f210c19
Filip Kozlicki <filip.kozlickii@gmail.com>
2022-05-13 16:43:21 +0200

    fallback to true

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -60,6 +60,6 @@ export async function getStaticPaths() {
 	const posts = await getPosts();
 	return {
 		paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
-		fallback: "blocking",
+		fallback: false,
 	};
 }
